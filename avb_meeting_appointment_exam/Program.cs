1127a9d02646be8f52cc8d545c6bfc6ded2e3977
3revisiae <erevisiae@gmail.com>
2024-03-17 21:39:33 +0800

    Add comment on emtpy meetingList

--- avb_meeting_appointment_exam/Program.cs
+++ avb_meeting_appointment_exam/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using avb_meeting_appointment_exam;
 List<Meeting> meetings = new List<Meeting>();
+//Current meetings is emtpy and prepopulated by CreateMockupMeetings()
 MeetingManager meetingManager = new MeetingManager(meetings);
 
 meetingManager.CreateMockupMeetings();
